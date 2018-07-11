bc5f5f3e59b39f1ecdea3e618c71f5f9a4fb64ac
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-11 16:59:12 -0700

    MyFirstSwingGUI, PhotoQuiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,33 +22,36 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+              String URL = "https://indywithkids.com/wp-content/uploads/2018/06/fireworks.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+              Component pic;
 		// 3. use the "createImage()" method below to initialize your Component
-
+              pic = createImage(URL);
 		// 4. add the image to the quiz window
-
+              quizWindow.add(pic);
 		// 5. call the pack() method on the quiz window
-
+              quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+              String input = JOptionPane.showInputDialog(null, "What is the color of the sky?(example:red)");
 		// 7. print "CORRECT" if the user gave the right answer
-
 		// 8. print "INCORRECT" if the answer is wrong
-
+                      if(input.equalsIgnoreCase("Blue")) {
+                    	  JOptionPane.showMessageDialog(null, "CORRECT!");
+                      }else {
+                    	  JOptionPane.showMessageDialog(null, "INCORRECT!");
+                      }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+              
 		// 10. find another image and create it (might take more than one line of code)
-
+              
 		// 11. add the second image to the quiz window
-
+              
 		// 12. pack the quiz window
-
+              
 		// 13. ask another question
-
+              
 		// 14+ check answer, say if correct or incorrect, etc.
-
+              
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
